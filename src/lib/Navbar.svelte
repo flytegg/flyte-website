d09c11a7ec14cb79978d2c21693e0e98796b88b2
Dan <29975530+DanIsLit@users.noreply.github.com>
2023-07-22 22:17:06 +0100

    Website development starter

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -1,13 +1,13 @@
 <section>
     <nav>
         <div class="flex bg-[#0D0B0D] py-[50px]">
-            <div class="flex mx-52 items-center">
+            <div class="flex items-center">
                 <div class="flex items-center space-x-2">
                     <img src="favicon.png" alt="Logo" class="h-5 w-5">
                     <a class="py-2 px-3 font-bold text-2xl text-white">Flyte</a>
                 </div>
             </div>
-            <ul class="flex items-center space-x-6 font-semibold text-[16px] text-white ml-auto mr-52">
+            <ul class="flex items-center space-x-6 font-semibold text-[16px] text-white ml-auto">
                 <li>
                     <a href="/#" class="py-2 px-3 rounded">Domain</a>
                 </li>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
     import "../app.css";
     import Navbar from "$lib/Navbar.svelte";
+    import Hero from "$lib/Hero.svelte";
+    import LatestProject from "$lib/LatestProject.svelte";
 </script>
 
-<Navbar />
-<slot />
+<div class="flex flex-col mx-52">
+    <Navbar />
+    <Hero />
+    <LatestProject />
+    <slot />
+</div>
