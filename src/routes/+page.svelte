0ed8e29a0386eb6ddb35e9df31e30b6f4c464e42
Stephen <138153996+stephendotgg@users.noreply.github.com>
2023-07-25 03:52:26 +0100

    Wireframe logo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,7 @@
 
 <section class="bg-[#0d0b0d] relative">
     <img src="/misc/blur.svg" alt="Blur" class="absolute top-0 w-[70%] z-30">
-    <img src="/misc/wireframe-logo.svg" alt="Blur" class="absolute right-16 top-[-230px] w-[800px] z-0">
+    <img src="/misc/wireframe-logo.svg" alt="Blur" class="hidden lg:block absolute right-16 top-[-230px] w-[800px] z-0">
     <div class="w-[90%] mx-auto">
         <Navbar />
     </div>
