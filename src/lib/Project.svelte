36f2bd045075495e2bd1c7fbff8048b17615cc4a
Stephen <138625055+stephendotgg@users.noreply.github.com>
2024-11-24 00:04:43 +0000

    change images

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -16,7 +16,7 @@
                     <p class="absolute bg-white/80 border-[1px] border-[#121012] rounded-md bottom-3 left-4 py-0.5 px-2 font-semibold text-sm text-[#121012]">Coming Soon</p>
                 </div>
             {:else}
-                <img src="{path}" alt="{alt}" class="rounded-t-[22px] w-full h-fit">
+                <img src="{path}" alt="{alt}" class="rounded-t-[22px] w-full">
             {/if}
             <div class="bg-[#181618] p-4 rounded-b-[22px] h-fit ">
                 <h2 class="text-[#b6b5b6] text-md pb-2">{description}</h2>
